e099540483a2170357e25c4eaa36de8c648910c9
Manuel Sande <mnlsande22@gmail.com>
2021-07-13 14:01:46 -0400

    rename and add addAccount(Account acct) method

--- src/Bank.java
+++ src/Bank.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
-public class Atm {
+public class Bank {
 
     private String name;
     private ArrayList<User> users;
     private ArrayList<Account> accounts;
 
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+    }
 }
