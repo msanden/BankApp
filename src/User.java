85891cc3cb2796d8af2b40ddc1da014dd6bf903c
Manuel Sande <mnlsande22@gmail.com>
2021-07-13 10:09:19 -0400

    created fields for the user class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
+/**
+ * User class representing a customer/client
+ * */
 
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private int pinNumber;
+    private String userID;
+    private ArrayList<Account> accounts;
+
 }
