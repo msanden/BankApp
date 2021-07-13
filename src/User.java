78dd72a0dd46a6892ab2dfc382904b8127d9639a
Manuel Sande <mnlsande22@gmail.com>
2021-07-13 11:17:45 -0400

    create constructor for user class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,7 @@
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.security.MessageDigest;
+
 
 /**
  * User class representing a customer/client
@@ -8,8 +11,29 @@ public class User {
 
     private String firstName;
     private String lastName;
-    private int pinNumber;
+    private byte[] pinNumber;
     private String userID;
     private ArrayList<Account> accounts;
+
+    /** creates a user with a specified name, pinNumber and their atm branch */
+    public User(String firstName, String lastName, String pinNumber, Atm atm) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        /** pin stored as a string hashed using md5 algorithm */
+        try {
+            MessageDigest cryptPin = MessageDigest.getInstance("MD5");
+            this.pinNumber = cryptPin.digest(pinNumber.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(" The cryptographic algorithm requested is not available in the environment");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        /**generate a userID for each user*/
+        this.userID = atm.getNewUserID();
+    }
+
+
 
 }
