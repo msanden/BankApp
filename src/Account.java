fbc94fda4039d3773b428ea2f850b207bd71e153
Manuel Sande <mnlsande22@gmail.com>
2021-07-13 13:51:15 -0400

    create card class, add constructor

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Account {
+    private String name;
+    private String accountID;
+    private User accHolder;
+    private ArrayList<Transaction> transactionHistory;
+
+    public Account(String name, User accHolder, Bank bank) {
+        this.name = name;
+        this.accHolder = accHolder;
+        this.accountID = bank.getAccountID();
+        this.transactionHistory = new ArrayList<Transaction>();
+        accHolder.addAccount(this);
+        bank.addAccount(this);
+    }
 }
