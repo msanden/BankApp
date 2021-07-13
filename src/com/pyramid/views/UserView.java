a1bc3dc9050cf28d643f037688399e904c66409b
Dmitrii Lutcenko <d.lutcenko2017@gmail.com>
2021-07-13 08:24:07 -0700

    Added UserView

--- src/com/pyramid/views/UserView.java
+++ src/com/pyramid/views/UserView.java
@@ -1,4 +1,11 @@
 package com.pyramid.views;
 
+import com.pyramid.entities.User;
+
 public class UserView {
+
+    public void printUser(User user) {
+        System.out.println("First name = " + user.getFirstName());
+        System.out.println("Last name = " + user.getLastName());
+    }
 }
